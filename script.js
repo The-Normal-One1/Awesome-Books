cf45622ca03d8188a23c835cdab07e2e702f04be
Ermi <manermidem@gmail.com>
2022-10-26 00:07:09 +0300

    Check for linters error

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Book Class:
+// Book Class:
 class Book {
   constructor(title, author) {
     this.title = title;
@@ -6,55 +6,12 @@ class Book {
   }
 }
 
-//UI class: Handles UI Tasks
-  class UI {
-    static displayBooks() {
-    const books = Store.getBooks();
-
-    books.forEach((book) => UI.addBookToList(book));
-    }
-
-    static addBookToList(book) {
-      const list = document.querySelector('#bookList');
-
-      const row = document.createElement('tr');
-      row.classList.add('row');
-
-      const createTitle = document.createElement('th');
-      const createAuthor = document.createElement('th');
-      createTitle.innerHTML = book.title;
-      createAuthor.innerHTML = book.author;
-      const createButton = document.createElement('th');
-      const removeBtn = document.createElement('button');
-      removeBtn.classList.add('remove-btn');
-      removeBtn.type = 'submit';
-      removeBtn.innerText = 'Remove';
-      createButton.appendChild(removeBtn);
-
-      row.appendChild(createTitle);
-      row.appendChild(createAuthor);
-      row.appendChild(createButton);
-      list.appendChild(row);
-    }
-
-    static deleteBook(el) {
-      if(el.classList.contains('remove-btn')) {
-        el.parentElement.parentElement.remove();
-      }
-    }
-
-    static clearfields() {
-      document.querySelector('#title').value = '';
-      document.querySelector('#author').value = '';
-    }
-  }
-
 // store classes: handles Storage
 
 class Store {
   static getBooks() {
     let books;
-    if(localStorage.getItem('books') === null) {
+    if (localStorage.getItem('books') === null) {
       books = [];
     } else {
       books = JSON.parse(localStorage.getItem('books'));
@@ -72,12 +29,55 @@ class Store {
     const books = Store.getBooks();
 
     books.forEach((book, index) => {
-      if(book.author === author) {
+      if (book.author === author) {
         books.splice(index, 1);
       }
     });
 
     localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+// UI class: Handles UI Tasks
+class UI {
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    books.forEach((book) => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector('#bookList');
+
+    const row = document.createElement('tr');
+    row.classList.add('row');
+
+    const createTitle = document.createElement('th');
+    const createAuthor = document.createElement('th');
+    createTitle.innerHTML = book.title;
+    createAuthor.innerHTML = book.author;
+    const createButton = document.createElement('th');
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('remove-btn');
+    removeBtn.type = 'submit';
+    removeBtn.innerText = 'Remove';
+    createButton.appendChild(removeBtn);
+
+    row.appendChild(createTitle);
+    row.appendChild(createAuthor);
+    row.appendChild(createButton);
+    list.appendChild(row);
+  }
+
+  static deleteBook(el) {
+    if (el.classList.contains('remove-btn')) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+
+  static clearfields() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
   }
 }
 
@@ -88,13 +88,13 @@ document.addEventListener('DOMContentLoaded', UI.displayBooks);
 // event: add books
 document.querySelector('#enter').addEventListener('submit', (e) => {
 // prevent defualt
-e.preventDefault();
+  e.preventDefault();
 
-  //get form values
+  // get form values
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
   // validation
-  if(title === '' || author === '') {
+  if (title === '' || author === '') {
     alert('Please fill in all fields');
   } else {
     // instatiate book
@@ -108,7 +108,6 @@ e.preventDefault();
     // clear field
     UI.clearfields();
   }
-
 });
 
 // event: remove a books
@@ -118,4 +117,4 @@ document.querySelector('#bookList').addEventListener('click', (e) => {
 
   // remove books form store
   Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
-})
+});
